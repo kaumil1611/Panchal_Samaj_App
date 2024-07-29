204b0cfec8efd8874ab4a425fa5a4a161b9e841a
kaumil-aspireedge <pkaumil16112000@gmail.com>
2024-07-29 18:48:50 +0530

    android changes to make build for android

--- android/app/src/main/java/com/panchalapp/MainActivity.kt
+++ android/app/src/main/java/com/panchalapp/MainActivity.kt
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle
+import org.devio.rn.splashscreen.SplashScreen // Import SplashScreen here
 
 class MainActivity : ReactActivity() {
 
@@ -12,7 +14,12 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "PanchalApp"
-
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    SplashScreen.show(this);
+  }
+  
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
